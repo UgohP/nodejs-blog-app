fc0ad9c2a32496742d110737cf2e832b9ddadbf3
Paschal Ugoh <paskyugoh@gmail.com>
2025-04-10 02:07:03 +0100

    finshed the nodejs blog app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 const cookieParser = require("cookie-parser");
 const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
 const session = require("express-session");
 const connectDB = require("./config/db");
 const app = express();
@@ -13,6 +14,8 @@ connectDB();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(methodOverride("_method"));
+
 // Populates req.session
 app.use(
   session({
--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Blog = require("../models/blog");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const adminLayout = "../views/layouts/admin";
 const jwtSecret = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.redirect("/admin");
+  }
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorised" });
+  }
+};
 
 router.get("/admin", async (req, res) => {
   try {
@@ -20,7 +35,16 @@ router.get("/admin", async (req, res) => {
 });
 
 router.get("/dashboard", async (req, res) => {
-  res.render("admin/dashboard");
+  try {
+    const locals = {
+      title: "Dashboard",
+      description: "A Nodejs Blog App built with Nodejs, Express and MongoDB",
+    };
+    const data = await Blog.find();
+    res.render("admin/dashboard", { locals, data, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 router.post("/login", async (req, res) => {
@@ -67,4 +91,73 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.get("/add-blog", async (req, res) => {
+  try {
+    const locals = {
+      title: "Add new blog",
+      description: "A Nodejs Blog App built with Nodejs, Express and MongoDB",
+    };
+    res.render("admin/add-blog", { locals, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/add-blog", async (req, res) => {
+  try {
+    try {
+      const { title, body } = req.body;
+      const newBlog = new Blog({
+        title: req.body.title,
+        body: req.body.body,
+      });
+      await Blog.create(newBlog);
+      res.redirect("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/edit-blog/:id", async (req, res) => {
+  try {
+    const locals = {
+      title: "Add new blog",
+      description: "A Nodejs Blog App built with Nodejs, Express and MongoDB",
+    };
+    const data = await Blog.findOne({ _id: req.params.id });
+    res.render("admin/edit-blog", { locals, data, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.put("/edit-blog/:id", async (req, res) => {
+  try {
+    await Blog.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now(),
+    });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/delete-blog/:id", async (req, res) => {
+  try {
+    await Blog.deleteOne({ _id: req.params.id });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/logout", async (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/admin");
+});
 module.exports = router;
